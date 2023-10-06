d3c5347283f9e6e5314a0440625924577164eecc
Brissle <145970256+Brissle@users.noreply.github.com>
2023-10-06 13:07:56 +0200

    Add files via upload
    Signed-off-by: Brissle <145970256+Brissle@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -2,85 +2,63 @@
 
 class Program
 {
-    static void Main(string[] args)
+/*
+ Grupp-4
+ Erik Brissle
+ Mats Johansson
+ */
+
+    static void Main()
     {
-        bool spelaIgen = true;
+        Console.Write("Ange ditt svenska personnummer (ÅÅMMDD-XXXX): ");
+        string Personnummer = Console.ReadLine(); 
+        //ber om ditt personnummer och sätter in i uträkningen
 
-        while (spelaIgen)
+        bool isValid = ValidateSwedishPersonalNumber(Personnummer); 
+        
+
+        if (isValid)
         {
-            Console.Write("Vad är din energinivå? Ange ett heltal: ");
-            if (int.TryParse(Console.ReadLine(), out int energiNivå))
+            Console.WriteLine("Giltigt personnummer.");
+        }
+        else
+        {
+            Console.WriteLine("Ogiltigt personnummer.");
+        }
+    }
+
+    static bool ValidateSwedishPersonalNumber(string Personnummer)
+    {
+        // Ta bort eventuella bindestreck eller mellanslag
+        Personnummer = Personnummer.Replace("-", "").Replace(" ", "");
+
+        // Kontrollera att personnumret har rätt längd (10 siffror)
+        if (Personnummer.Length != 10)
+        {
+            return false;
+        }
+
+        // Konvertera personnumret till en array av heltal
+        int[] digits = new int[10];
+        for (int i = 0; i < 10; i++)
+        {
+            if (!int.TryParse(Personnummer[i].ToString(), out digits[i]))
             {
-                int distans = 0;
-                while (distans < 10)
-                {
-                    Console.WriteLine($"Du har {energiNivå} energi och har täckt {distans} km.");
-                    Console.Write("Välj din hastighet (snabbast/snabb/måttlig/långsam/promenad): ");
-                    string hastighet = Console.ReadLine().ToLower();
-
-                    int energiKostnad = 0;
-                    int avståndTäckt = 0;
-
-                    switch (hastighet)
-                    {
-                        case "snabbast":
-                            energiKostnad = 15;
-                            avståndTäckt = 2;
-                            break;
-                        case "snabb":
-                            energiKostnad = 10;
-                            avståndTäckt = 1;
-                            break;
-                        case "måttlig":
-                            energiKostnad = 8;
-                            avståndTäckt = 1;
-                            break;
-                        case "långsam":
-                            energiKostnad = 5;
-                            avståndTäckt = 1;
-                            break;
-                        case "promenad":
-                            energiKostnad = 2;
-                            avståndTäckt = 1;
-                            break;
-                        default:
-                            Console.WriteLine("Ogiltig hastighet. Försök igen.");
-                            break;
-                    }
-
-                    if (energiNivå >= energiKostnad)
-                    {
-                        energiNivå -= energiKostnad;
-                        distans += avståndTäckt;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Du har inte tillräckligt med energi för den hastigheten.");
-                        break; // Börja om spelet om energin tar slut
-                    }
-                }
-
-                if (distans >= 10)
-                {
-                    Console.WriteLine("Grattis! Du har nått 10 km och vunnit spelet!");
-                }
-                else
-                {
-                    Console.WriteLine($"Du har tagit slut på energi efter att ha täckt {distans} km.");
-                }
-
-                Console.Write("Vill du spela igen (ja/nej)? ");
-                string svar = Console.ReadLine();
-
-                if (svar.ToLower() != "ja")
-                {
-                    spelaIgen = false;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Ogiltig inmatning av energinivå.");
+                return false; //  Beräkna att varje siffra blir ett heltal, eller kolla felaktikt tecken
             }
         }
+
+        // Beräkna kontrollsiffran
+        int sum = 0;
+        for (int i = 0; i < 9; i++)
+        {
+            int product = digits[i] * ((i % 2 == 0) ? 2 : 1);
+            sum += (product > 9) ? product - 9 : product;
+        }
+
+        int checksum = (10 - (sum % 10)) % 10;
+
+        // Jämför kontrollsiffran med den sista siffran i personnumret
+        return checksum == digits[9];
     }
 }
